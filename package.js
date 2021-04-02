21419107b9323aa08ca079c9e6d04a7bc705975f
Harry Adel <harryadelb@gmail.com>
2021-04-03 00:04:35 +0200

    Remove package.js

--- package.js
+++
@@ -1,6 +0,0 @@
-Package.onUse(function (api) {
-  // If we are loading mongo-livedata, let you store ObjectIDs in it.
-  api.use(['mongo', 'reload'], { weak: true });
-  api.mainModule('migration.js');
-  api.export('ReactiveDict');
-});
