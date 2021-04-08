746a09625c6df72dcc5a833bbe80e412e8429cb8
Harry Adel <harryadelb@gmail.com>
2021-04-08 17:17:37 +0200

    Comment out migration file

--- src/migration.js
+++ src/migration.js
@@ -1,21 +1,19 @@
+// if (Meteor.isClient && Package.reload) {
+//   // Put old migrated data into ReactiveDict._migratedDictData,
+//   // where it can be accessed by ReactiveDict._loadMigratedDict.
+//   const migrationData = Package.reload.Reload._migrationData('reactive-dict');
+//   if (migrationData && migrationData.dicts)
+// { ReactiveDict._migratedDictData = migrationData.dicts; }
 
+//   // On migration, assemble the data from all the dicts that have been
+//   // registered.
+//   Package.reload.Reload._onMigrate('reactive-dict', () => {
+//     const dictsToMigrate = ReactiveDict._dictsToMigrate;
+//     const dataToMigrate = {};
 
-if (Meteor.isClient && Package.reload) {
-  // Put old migrated data into ReactiveDict._migratedDictData,
-  // where it can be accessed by ReactiveDict._loadMigratedDict.
-  var migrationData = Package.reload.Reload._migrationData('reactive-dict');
-  if (migrationData && migrationData.dicts)
-    ReactiveDict._migratedDictData = migrationData.dicts;
-
-  // On migration, assemble the data from all the dicts that have been
-  // registered.
-  Package.reload.Reload._onMigrate('reactive-dict', function () {
-    var dictsToMigrate = ReactiveDict._dictsToMigrate;
-    var dataToMigrate = {};
-
-    for (var dictName in dictsToMigrate)
-      dataToMigrate[dictName] = dictsToMigrate[dictName]._getMigrationData();
-
-    return [true, {dicts: dataToMigrate}];
-  });
-}
+//     for (const dictName in dictsToMigrate) {
+//  dataToMigrate[dictName] = dictsToMigrate[dictName]._getMigrationData();
+// }
+//     return [true, { dicts: dataToMigrate }];
+//   });
+// }
