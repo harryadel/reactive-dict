817f789e541558826c3c250a7851af8d7c629efa
Harry Adel <harryadelb@gmail.com>
2021-04-28 19:04:23 +0200

    lint: use local variable instead of argument

--- src/mongoId.js
+++ src/mongoId.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 const MongoID = {};
 
 MongoID._looksLikeObjectID = (str) => str.length === 24 && str.match(/^[0-9a-f]*$/);
@@ -6,12 +7,12 @@ MongoID.ObjectID = class ObjectID {
   constructor(hexString) {
     // random-based impl of Mongo ObjectID
     if (hexString) {
-      hexString = hexString.toLowerCase();
-      if (!MongoID._looksLikeObjectID(hexString)) {
+      const lowerCaseHexString = hexString.toLowerCase();
+      if (!MongoID._looksLikeObjectID(lowerCaseHexString)) {
         throw new Error('Invalid hexadecimal string for creating an ObjectID');
       }
       // meant to work with _.isEqual(), which relies on structural equality
-      this._str = hexString;
+      this._str = lowerCaseHexString;
     } else {
       this._str = Random.hexString(24);
     }
